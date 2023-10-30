77b335ef129674d634db01d45835617acc09d3fb
Vladislav <vzhalin81@gmail.com>
2023-10-30 18:46:13 +0300

    added error

--- src/api.js
+++ src/api.js
@@ -2,6 +2,11 @@ export async function getPlaylist() {
   const response = await fetch(
     "https://skypro-music-api.skyeng.tech/catalog/track/all/"
   );
+
+  if (!response.ok) {
+    throw new Error("Ошибка сервера");
+  }
+
   const data = await response.json();
-  return data; 
+  return data;
 }
